b005656fe0960e8dc7863b34ff7601aa03068f82
guilherme Rocha <guilherme.rocha@absoftware.com.br>
2020-08-09 02:01:01 -0300

    commit inicial do projeto

---
+++ guilherme/src/main/java/com/guilherme/web/GuilhermeApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilhermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuilhermeApplication.class, args);
+	}
+
+}
---
+++ guilherme/src/test/java/com/guilherme/web/GuilhermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilhermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
