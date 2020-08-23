43a7fa7a701353bb0a39a1bda77b3355457f10e3
guilherme Rocha <guilherme.rocha@absoftware.com.br>
2020-08-23 02:25:49 -0300

    ManyToMany (muitos-para-muitos) c JoinTable  entre Category e Product

--- guilherme/src/main/java/com/guilherme/web/entities/Category.java
+++ guilherme/src/main/java/com/guilherme/web/entities/Category.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -23,9 +24,7 @@ public class Category implements Serializable{
 	private Long id;
 	private String name;
 	
-	//@JsonIgnore
-	//@OneToMany(mappedBy = "categories")
-	@Transient
+	@ManyToMany(mappedBy = "categories")
 	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
